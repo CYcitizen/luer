40fa3703ef709c9d6f7f8473f9e3f1601f7ee5b5
jokerclay <2079209547@qq.com>
2021-11-07 23:59:11 +0800

    server test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1 class="test">HelloWorld</h1>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+
+    }
   }
+
+  // components: {
+  //   HelloWorld
+  // }
+
 }
 </script>
 
+
+
+
+
+
 <style>
-#app {
+*{
+  margin: 0;
+  padding: 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#app {
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  /* text-align: center; */
+  /* color: #2c3e50;
+  margin-top: 60px; */
+
+}
+.test{
+  color: red;
+  font-size: 1.5rem;
 }
 </style>
